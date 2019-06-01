e1ffe8b1657bd20df26eb367d849b6e10a2ffdb7
v13nr2 <nanang.programmer@gmail.com>
2019-06-02 06:41:11 +0800

    *ngFor for Class COA

--- coa/src/app/angular/list-coas/list-coas.component.ts
+++ coa/src/app/angular/list-coas/list-coas.component.ts
@@ -10,10 +10,15 @@ export class ListCoasComponent implements OnInit {
   constructor() { }
 
   coas = [
+    new coa(1,'A0001','Aktiva',true,new Date()),
+    new coa(2,'P0001','Passiva',true,new Date()),
+    new coa(3,'L0001','Laba Rugi',true,new Date()),
+    
+    /*
     {id:1, kode_coa: 'A0001', nama_coa:'Aktiva'},
     {id:2, kode_coa: 'P0001', nama_coa:'Pasiva'},
     {id:3, kode_coa: 'L0001', nama_coa:'Laba Rugi'},
-    
+    */
   ]
   coa = {
     id:1,
@@ -24,4 +29,16 @@ export class ListCoasComponent implements OnInit {
   ngOnInit() {
   }
 
+}
+
+export class coa{
+  constructor(
+    public id: number,
+    public kode_coa: string,
+    public nama_coa: string,
+    public active: boolean,
+    public created_date: Date
+  ){
+
+  }
 }
